3d1933996c625fde2b33f0fabda4c8cf7d8fa642
James C. Thomas <jcthomas531@gmail.com>
2025-09-10 14:08:52 -0500

    initial version of kable function script

---
+++ kableFuns.R
@@ -0,0 +1,25 @@
+library(kableExtra)
+
+
+################################################################################
+#very basic table formatting for a pdf document
+basicKablePdf <- function(table, 
+                          title = "title",
+                          colNames = NULL,
+                          digits = NULL) {
+  #argument set up
+  kableArgs <- list(x = table, caption = title,
+                    longtable=FALSE, format = "latex")
+  if (!is.null(colNames)) kableArgs$col.names <- colNames
+  if (!is.null(digits)) kableArgs$digits = digits
+  #table creation
+  retTab <- do.call(kable, kableArgs) |>
+    kable_styling(latex_options = c("HOLD_position", "scale_down")) |>
+    column_spec(1,bold=TRUE) |>
+    row_spec(0, bold = TRUE) 
+  
+  
+  #since this returns a kable object, you can pipe it into other kable extra funs
+  return(retTab)
+  
+}
